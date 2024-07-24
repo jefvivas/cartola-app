51c0a74cbd280c38efd4b040fd196b1367b81251
Jeferson <jefvivasaragao@gmail.com>
2024-07-24 16:30:22 -0300

    changes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./HomeScreen";
+import { View, Text } from "react-native";
+
 import TeamDetailsScreen from "./TeamDetails";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
+const FirstScreen = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+const NotificationsScreen = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Notifications Screen</Text>
+  </View>
+);
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
